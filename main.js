3549419c2559a82f75db77f12369a55d64b9af0c
Saar Levy <saar.levy0@gmail.com>
2017-07-22 19:16:01 +0300

    All buttons now play audio, small changes to style and markup

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-const clap = new Audio("./sounds/clap.wav");
-$("#clapButton").click(() => {
-    clap.currentTime = 0.125; // Temporary, soundfile has a bit of nothing at the beginning 
-    clap.play();
+console.log($('button'));
+
+$('button').click((e) => {
+    // Getting audio src and playing through js audio is faster than playing html5 audio
+    const audioSrc = $('#'+e.currentTarget.id).find('audio').attr('src');
+    const audio = new Audio(audioSrc);
+    audio.play();
 });
