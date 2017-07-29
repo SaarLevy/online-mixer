59a2d96d1fe5c01d0df7e150b0171037a28b5080
Saar Levy <saar.levy0@gmail.com>
2017-07-29 15:49:27 +0300

    buttons are created dynamically and react to mouse clicks

--- main.js
+++ main.js
@@ -1,8 +1,54 @@
-console.log($('button'));
+let buttons = [
+    {
+        name: 'Bassdrum',
+        audio:'bassdrum.wav',
+    },
+    {
+        name: 'Clap',
+        audio: 'clap.wav',
+    },
+    {
+        name: 'Closed HiHat',
+        audio: 'closed-hihat.wav'
+    },
+    {
+        name: 'Open HiHat',
+        audio: 'open-hihat.wav'
+    },
+    {
+        name: 'Cowbell',
+        audio: 'cowbell.wav'
+    },
+    {
+        name: 'Crash',
+        audio: 'crash.wav'
+    },
+    {
+        name: 'Ride',
+        audio: 'ride.wav'
+    },
+    {
+        name: 'Snare',
+        audio: 'snare.wav'
+    }
+]
 
-$('button').click((e) => {
-    // Getting audio src and playing through js audio is faster than playing html5 audio
-    const audioSrc = $('#'+e.currentTarget.id).find('audio').attr('src');
-    const audio = new Audio(audioSrc);
-    audio.play();
+$(document).ready(() => {
+    buttons.forEach((button) => {
+        $('#sounds').append(
+            `
+            <button class="audio-button">
+                <audio src='./sounds/${button.audio}'></audio>
+                ${button.name}
+            </button>
+            `
+        )
+    });
+    $('button').on('click', (e) => {
+        // Getting audio src and playing through js audio is faster than playing html5 audio
+        console.log('hello')
+        const audioSrc = $(e.currentTarget).find('audio').attr('src')
+        const audio = new Audio(audioSrc);
+        audio.play();
+    });
 });
